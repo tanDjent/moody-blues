425d8e072a6a6730d231d51005ad35d0b9e9d6a0
Tanmay <vermatanmay@hotmail.com>
2024-12-11 01:33:40 +0530

    prisma and Supabase setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,27 @@
-import Link from "next/link";
+import Link from "next/link"
+import { auth } from "@clerk/nextjs/server"
 
-export default function Home() {
-  return <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
-    <div className="w-full max-w-[600px] mx-auto">
-      <h1 className="text-6xl mb-4">{`Moody Blues, Your mind's journey`}</h1>
-      <p className="text-2xl text-white/60 mb-4">This is the best app for tracking your mood throughout your life. All you have to do is be honest.</p>
-      <div>
-        <Link href="/journal">
-        <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl">Get Started</button>
-        </Link>
+export default async function Home() {
+  const { userId } = await auth()
+
+  let href = userId ? "/journal" : "/new-user"
+
+  return (
+    <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
+      <div className="w-full max-w-[600px] mx-auto">
+        <h1 className="text-6xl mb-4">{`Moody Blues, Your mind's journey`}</h1>
+        <p className="text-2xl text-white/60 mb-4">
+          This is the best app for tracking your mood throughout your life. All
+          you have to do is be honest.
+        </p>
+        <div>
+          <Link href={href}>
+            <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl">
+              Get Started
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
-  </div>
+  )
 }
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,6 +1,6 @@
 import { SignUp } from "@clerk/nextjs"
 
 const SignUpPage = () => {
-  return <SignUp />
+  return <SignUp forceRedirectUrl={"/new-user"} />
 }
 export default SignUpPage
